d1c4206f5f60c0355fa440e0ef350914600bf5da
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-12-09 22:25:33 -0300

    Função para realizar a subtração

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Soma();
+// Soma();
+Subtracao();
 
 static void Soma()
 {
@@ -19,4 +20,23 @@ static void Soma()
     Console.WriteLine($"O resultado da soma é {resultado}");
     // Console.WriteLine($"O resultado da soma é {v1 + v2}");
     // Console.WriteLine("O resultado da soma é " + (v1 + v2));
+
+    Console.ReadKey();
+}
+
+static void Subtracao()
+{
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    var resultado = v1 - v2;
+    Console.WriteLine($"O resultado da subtração é {resultado}");
+    Console.ReadKey();
 }
