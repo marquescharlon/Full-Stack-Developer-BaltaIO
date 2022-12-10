4794e8abc57ce6e78b1c2c4cdd28ae96751ee444
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-12-09 22:50:55 -0300

    Chamando funções e saindo da aplicação

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Soma();
-// Subtracao();
-// Divisao();
-Multiplicacao();
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que deseja fazer?");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Divisão");
+    Console.WriteLine("4 - Multiplicação");
+    Console.WriteLine("5 - Sair");
+
+    Console.WriteLine("-------------");
+    Console.WriteLine("Selecione uma opção: ");
+    short res = short.Parse(Console.ReadLine());
+
+    switch (res)
+    {
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Divisao(); break;
+        case 4: Multiplicacao(); break;
+        case 5: System.Environment.Exit(0); break;
+        default: Menu(); break;
+    }
+}
 
 static void Soma()
 {
@@ -24,6 +46,7 @@ static void Soma()
     // Console.WriteLine("O resultado da soma é " + (v1 + v2));
 
     Console.ReadKey();
+    Menu();
 }
 
 static void Subtracao()
@@ -41,6 +64,7 @@ static void Subtracao()
     var resultado = v1 - v2;
     Console.WriteLine($"O resultado da subtração é {resultado}");
     Console.ReadKey();
+    Menu();
 }
 
 static void Divisao()
@@ -58,6 +82,7 @@ static void Divisao()
     var resultado = v1 / v2;
     Console.WriteLine($"O resultado da divisão é {resultado}");
     Console.ReadKey();
+    Menu();
 }
 
 static void Multiplicacao()
@@ -75,4 +100,5 @@ static void Multiplicacao()
     var resultado = v1 * v2;
     Console.WriteLine($"O resultado da multiplicação é {resultado}");
     Console.ReadKey();
+    Menu();
 }
