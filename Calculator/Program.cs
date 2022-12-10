b59d002b23994615a227225eb9b6f6bda0e737c6
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-12-09 22:19:02 -0300

    Utilizando funções

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,17 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Clear();
+Soma();
 
-Console.WriteLine("Primeiro valor: ");
-float v1 = float.Parse(Console.ReadLine());
+static void Soma()
+{
+    Console.Clear();
 
-Console.WriteLine("Segundo valor: ");
-float v2 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
 
-Console.WriteLine("");
+    Console.WriteLine("Segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
 
-var resultado = v1 + v2;
-Console.WriteLine("O resultado da soma é " + resultado);
-Console.WriteLine($"O resultado da soma é {resultado}");
-Console.WriteLine($"O resultado da soma é {v1 + v2}");
-Console.WriteLine("O resultado da soma é " + (v1 + v2));
+    Console.WriteLine("");
+
+    var resultado = v1 + v2;
+    // Console.WriteLine("O resultado da soma é " + resultado);
+    Console.WriteLine($"O resultado da soma é {resultado}");
+    // Console.WriteLine($"O resultado da soma é {v1 + v2}");
+    // Console.WriteLine("O resultado da soma é " + (v1 + v2));
+}
