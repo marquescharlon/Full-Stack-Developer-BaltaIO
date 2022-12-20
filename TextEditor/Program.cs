40ce3c00ffcac9f195e754674e932f94ce72a190
Marquescharlon Santos <marquescharlonrs@gmail.com>
2022-12-20 00:47:43 -0300

    Criando uma nova aplicação TextEditor (Editor de texto)

---
+++ TextEditor/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que você deseja fazer?");
+        Console.WriteLine("1 - Abrir arquivo");
+        Console.WriteLine("2 - Criar novo arquivo");
+        Console.WriteLine("0 - Sair");
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0: System.Environment.Exit(0); break;
+            case 1: Abrir(); break;
+            case 2: Editar(); break;
+            default: Menu(); break;
+        }
+    }
+
+    static void Abrir() { }
+
+    static void Editar()
+    {
+        Console.Clear();
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+        Console.WriteLine("----------------");
+        string text = "";
+
+        while (Console.ReadKey().Key != ConsoleKey.Escape)
+    }
+}
