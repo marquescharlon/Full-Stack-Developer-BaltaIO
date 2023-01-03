f05e0f67bb8a1df36d70ce6a36bd090712133976
Marquescharlon Santos <marquescharlonrs@gmail.com>
2023-01-02 22:08:07 -0300

    Criando novo projeto 'EditorHtml'

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,23 @@
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+            Console.Write("+");
+        }
+    }
+}
---
+++ EditorHtml/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace EditorHtml
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
+
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+
+//     }
+// }
