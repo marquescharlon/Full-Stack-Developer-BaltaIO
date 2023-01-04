e424907cb19309546f5da42640fc715925b8f060
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2023-01-04 13:42:20 -0300

    Novas formatações ao menu

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -10,6 +10,9 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
         }
 
         public static void DrawScreen()
@@ -17,7 +20,43 @@ namespace EditorHtml
             Console.Write("+");
             for (int i = 0; i <= 30; i++)
                 Console.Write("-");
+
             Console.Write("+");
+            Console.Write("\n");
+
+            for(int lines = 0; lines <= 10; lines++){
+                Console.Write("|");
+                for(int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("=============");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir ");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair ");
+            Console.SetCursorPosition(3,10);
+            Console.WriteLine("Opção: ");
         }
     }
 }
