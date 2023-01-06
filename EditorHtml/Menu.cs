69a15a59d53072021bb89c910894c3e057e3c2b7
Marquescharlon Santos <marquescharlonrs@gmail.com>
2023-01-05 22:44:14 -0300

    Finalizando Mão na massa: Editor HTML

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -13,6 +13,7 @@ namespace EditorHtml
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -24,9 +25,10 @@ namespace EditorHtml
             Console.Write("+");
             Console.Write("\n");
 
-            for(int lines = 0; lines <= 10; lines++){
+            for (int lines = 0; lines <= 10; lines++)
+            {
                 Console.Write("|");
-                for(int i = 0; i <= 30; i++)
+                for (int i = 0; i <= 30; i++)
                     Console.Write(" ");
 
                 Console.Write("|");
@@ -43,20 +45,36 @@ namespace EditorHtml
 
         public static void WriteOptions()
         {
-            Console.SetCursorPosition(3,2);
+            Console.SetCursorPosition(3, 2);
             Console.WriteLine("Editor HTML");
-            Console.SetCursorPosition(3,3);
+            Console.SetCursorPosition(3, 3);
             Console.WriteLine("=============");
-            Console.SetCursorPosition(3,4);
+            Console.SetCursorPosition(3, 4);
             Console.WriteLine("Selecione uma opção abaixo");
-            Console.SetCursorPosition(3,6);
+            Console.SetCursorPosition(3, 6);
             Console.WriteLine("1 - Novo arquivo");
-            Console.SetCursorPosition(3,7);
+            Console.SetCursorPosition(3, 7);
             Console.WriteLine("2 - Abrir ");
-            Console.SetCursorPosition(3,9);
+            Console.SetCursorPosition(3, 9);
             Console.WriteLine("0 - Sair ");
-            Console.SetCursorPosition(3,10);
+            Console.SetCursorPosition(3, 10);
             Console.WriteLine("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
         }
     }
 }
