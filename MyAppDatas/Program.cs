14357a9671f09e53d80d49d8c497eab2464935ad
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2023-01-09 13:41:50 -0300

    Formatando datas

---
+++ MyAppDatas/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+// // var data = new DateTime();
+// var data = DateTime.Now;
+// Console.WriteLine(data);
+// Console.WriteLine(data.Year);
+// Console.WriteLine(data.Month);
+// Console.WriteLine(data.Day);
+// Console.WriteLine(data.Hour);
+// Console.WriteLine(data.Minute);
+// Console.WriteLine(data.Second);
+
+// Console.WriteLine((int)data.DayOfWeek);
+// Console.WriteLine((int)data.DayOfYear);
+
+var data = DateTime.Now;
+var formato = String.Format("{0}", data);
+Console.WriteLine(formato);
